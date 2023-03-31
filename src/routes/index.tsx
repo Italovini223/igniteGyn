b4cd7213533e241cfdc9ea7b35907c37ce354094
Italovini223 <italovinicius2018@gmail.com>
2023-03-30 22:06:02 -0300

    ✨ feat: code in Rotas privadas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,8 @@ import { useTheme, Box } from 'native-base'
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native'
 
 import { AuthRoutes } from './auth.routes'
+import { AppRoutes } from './app.routes'
+
 
 export function Routes(){
 
@@ -18,7 +20,7 @@ export function Routes(){
       bg="gray.700"
     >
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        <AppRoutes />
       </NavigationContainer>
     </Box>
   )

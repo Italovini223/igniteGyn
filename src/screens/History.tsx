7cf7a0a44bbe66d4f9c61bf82ffe176eb96724aa
Italovini223 <italovinicius2018@gmail.com>
2023-05-03 21:11:24 -0300

    ✨ code in step 5 (2)

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,43 @@
-import { Center, Text} from 'native-base'
+import { useState } from 'react'
+
+
+import { Heading, VStack, SectionList } from 'native-base'
+
+import { HistoryCard } from '@components/HistoryCard'
+import { ScreenHeader } from '@components/ScreenHeader'
 
 export function History(){
+  const [exercises, setExercises] = useState([
+    {
+      title: '26.08.22',
+      data: ['Puxada frontal', 'Remada unilateral'],
+    },
+    {
+      title: '27.08.22',
+      data: ['puxada frontal']
+    }
+  ])
+
   return (
-    <Center flex={1}>
-      <Text>History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader 
+        title="Histórico de Exercícios"
+      />
+
+      <SectionList 
+        sections={exercises}
+        keyExtractor={(item, index) => item + index}
+        renderSectionHeader={({ section }) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        px={4}
+      />
+
+    </VStack>
   )
 }
